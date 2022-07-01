d0e07348c9626f8ade3c415dc499f9a417256dc9
Fellipe Corominas <lefelps@hotmail.com>
2022-07-01 16:52:16 -0300

    some initial connection tests and first database configuration with challenge related tables

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,11 @@
-import mysql from mysql
+import mysql from 'mysql'
 import 'dotenv/config'
 
-const connectDB = () => {
-    return mysql.createConnection({
-        host: 'localhost',
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: 'koalify'
-    });
-}
+const connectDB = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'koalify'
+});
 
 export default connectDB
--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import 'dotenv/config'
 
+import connectDB from './database/connection.js'
 
 import usersRoutes from './routes/users.js'
+
+import mysql from 'mysql'
 
 const app = express();
 const PORT = 5000;
@@ -12,8 +15,23 @@ app.use(bodyParser.json())
 
 app.use('/users', usersRoutes)
 
-app.get('/', (req, res) => {
-    res.send("Hello form homepage")
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'koalify'
 });
+
+
+app.get('/', (req, res) => {
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM categories", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Server running on PORT: http://localhost:${PORT}`))
