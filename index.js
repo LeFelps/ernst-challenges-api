c679cd98a75f50249103b7d78f9dbe5432ce1f4f
Fellipe Corominas <lefelps@hotmail.com>
2022-07-17 16:11:49 -0300

    adjustments to databse sql config file

--- index.js
+++ index.js
@@ -2,14 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import 'dotenv/config'
 
-import connectDB from './database/connection.js'
-
 import usersRoutes from './routes/users.js'
 import challengesRoutes from './routes/challenges.js'
 import jobsRoutes from './routes/jobs.js'
 import opponentsRoutes from './routes/opponents.js'
-
-
 
 const app = express();
 const PORT = 5000;
@@ -27,11 +23,5 @@ app.use('/users', usersRoutes)
 app.use('/challenges', challengesRoutes)
 app.use('/jobs', jobsRoutes)
 app.use('/opponents', opponentsRoutes)
-
-
-
-app.get('/', (req, res) => {
-});
-
 
 app.listen(PORT, () => console.log(`Server running on PORT: http://localhost:${PORT}`))
