478cfc3e6b3d0ed81ac1301eec878ab4e85354ad
Fellipe Corominas <lefelps@hotmail.com>
2022-07-21 20:10:05 -0300

    minor adjustments to users endpoints

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,20 @@ import connectDB from '../database/connection.js'
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send(users)
+    const con = connectDB()
     con.query("SELECT * FROM users WHERE username = ? AND password = ?", [req.username, req.password], function (err, result, fields) {
         if (err) throw err;
-        challenge = { ...challenge, ...result }
+
+        if (result.length > 0) res.send(true)
+        else res.send(false)
+    });
+})
+
+router.post('/', (req, res) => {
+    const con = connectDB()
+    con.query("INSERT INTO users (email, password, username) VALUES (?, ?, ?)", [req.username, req.password], function (err, result, fields) {
+        if (err) throw err;
+
     });
 })
 
