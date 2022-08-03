f8dfdf24da80c80157b2ca9c15c38a0f9cb6799a
Fellipe Corominas <lefelps@hotmail.com>
2022-08-02 21:08:42 -0300

    attempts to fix challenge post return value + successfull response for challenge by Id endpoint

--- routes/challenges.js
+++ routes/challenges.js
@@ -29,44 +29,93 @@ router.post('/', (req, res) => {
 
                 challenge.id = result.insertId
 
-                challenge.questions?.length > 0 ?
-                    challenge.questions.map((question, q_index) => {
-                        con.query("INSERT INTO questions (challengeId, title, type, level) VALUES (?, ?, ?, ?)",
-                            [result.insertId, question.title, question.type, question.level],
-                            function (err, result, fields) {
-                                if (err) throw err;
-                                challenge.questions[q_index].id = result.insertId
-                                if (question?.options?.length > 0)
-                                    question.options.map((option, o_index) => {
-                                        con.query("INSERT INTO answers (questionId, type, value, correctAnswer) VALUES (?, ?, ?, ?)",
-                                            [result.insertId, option.type, option.value, option.correctAnswer],
+                Promise.all([
+                    new Promise((resolve, reject) => {
+                        challenge.questions?.length > 0 ?
+                            Promise.all([
+                                challenge.questions.map((question, q_index) => {
+                                    return new Promise((resolve, reject) => {
+                                        con.query("INSERT INTO questions (challengeId, title, type, level) VALUES (?, ?, ?, ?)",
+                                            [result.insertId, question.title, question.type, question.level],
                                             function (err, result, fields) {
-                                                if (err) throw err;
-                                                challenge.questions[q_index].options[o_index].id = result.insertId
+                                                if (err) reject(err);
+
+                                                challenge.questions[q_index].id = result.insertId
+                                                let answerList = []
+                                                if (question?.answers?.length > 0) {
+                                                    question.answers.map((answer, a_index) => {
+                                                        answerList.push([result.insertId, answer.type, answer.value, answer.correctAnswer])
+                                                    })
+                                                    console.log(answerList)
+                                                    con.query(`INSERT INTO answers (questionId, type, value, correctAnswer) VALUES ${answerList.map(answer => (
+                                                        `(${answer[0]},'${answer[1]}','${answer[2]}',${answer[3]})`
+                                                    ))}`,
+                                                        function (err, result, fields) {
+                                                            if (err) reject(err);
+                                                            resolve()
+                                                        });
+                                                }
                                             });
+                                    }).then(response => {
+                                        resolve()
+                                    }).catch(error => {
+                                        reject()
                                     })
-                            });
-                    })
-                    : challenge.questions = []
-                challenge.checkpoints?.length > 0 ?
-                    challenge.checkpoints.map((checkpoint, c_index) => {
-                        con.query("INSERT INTO checkpoints (challengeId, description, technologies) VALUES (?, ?, ?)",
-                            [result.insertId, checkpoint.description, checkpoint.technologies ? checkpoint.technologies.join(';') : ""],
-                            function (err, result, fields) {
-                                if (err) throw err;
-                                challenge.checkpoints[c_index].id = result.insertId
-                                if (checkpoint.references?.length > 0)
-                                    checkpoint.references.map((reference, r_index) => {
-                                        con.query("INSERT INTO sources (checkpointId, title, link) VALUES (?, ?, ?)",
-                                            [result.insertId, reference.title, reference.link],
+                                })
+                            ])
+                                .then(response => {
+                                    resolve()
+                                })
+                                .catch(error => {
+                                    reject()
+                                })
+                            : resolve([])
+                    }),
+                    new Promise((resolve, reject) => {
+                        challenge.checkpoints?.length > 0 ?
+                            Promise.all([
+                                challenge.checkpoints.map((checkpoint, c_index) => {
+                                    return new Promise((resolve, reject) => {
+                                        con.query("INSERT INTO checkpoints (challengeId, description, technologies) VALUES (?, ?, ?)",
+                                            [result.insertId, checkpoint.description, checkpoint.technologies ? checkpoint.technologies.join(';') : ""],
                                             function (err, result, fields) {
-                                                if (err) throw err;
-                                                challenge.checkpoints[c_index].references[r_index].id = result.insertId
+                                                if (err) reject(err);
+                                                challenge.checkpoints[c_index].id = result.insertId
+                                                let referenceList = []
+                                                if (checkpoint.references?.length > 0) {
+                                                    checkpoint.references.map((reference, r_index) => {
+                                                        referenceList.push([result.insertId, reference.title, reference.link])
+                                                        challenge.checkpoints[c_index].id = result.insertId
+                                                    })
+                                                    con.query(`INSERT INTO sources (checkpointId, title, link) VALUES ${referenceList.map(reference => (
+                                                        `(${reference[0]},'${reference[1]}','${reference[2]}')`
+                                                    ))}`,
+                                                        function (err, result, fields) {
+                                                            if (err) reject(err);
+                                                            resolve(result)
+                                                        });
+                                                }
                                             });
+                                    }).then(response => {
+                                        resolve()
+                                    }).catch(error => {
+                                        reject()
                                     })
-                            });
+                                })
+                            ])
+                                .then(response => {
+                                    resolve()
+                                })
+                                .catch(error => {
+                                    reject()
+                                })
+                            : resolve([])
                     })
-                    : challenge.checkpoints = []
+                ]).then(resp => {
+                    res.send(challenge)
+                }).catch(err => {
+
+                })
             })
     });
 })
@@ -215,45 +264,90 @@ router.get('/:id', (req, res) => {
         checkpoints: [],
         questions: []
     }
-    new Promise((resolve, reject) => {
-        con.connect(function (err) {
-            if (err) throw err;
 
-            con.query("SELECT * FROM challenges WHERE id = ?", [req.params['id']], function (err, result, fields) {
-                if (err) throw err;
-                challenge = { ...result }
-                con.query(`SELECT * FROM questions WHERE challengeId = ${req.params['id']}`, function (err, result, fields) {
-                    if (err) throw err;
-                    challenge.questions = result
-                    con.query(`SELECT * FROM answers ${result.map((question, index) => (
-                        `${index !== 0 ? "OR" : "WHERE"} questionId = ${question.id}`
-                    ))
-                        }`, function (err, result, fields) {
-                            if (err) throw err;
-                            let answers = result
-                            challenge?.questions.map((question, index) => {
-                                challenge.questions[index] = answers.filter(a => a.questionId === question.id)
-                            })
-                        });
-                });
-                con.query(`SELECT * FROM checkpoints WHERE challengeId = ${req.params['id']}`, function (err, result, fields) {
-                    if (err) throw err;
-                    challenge.checkpoints = result
-                    con.query(`SELECT * FROM sources ${result.map((checkpoint, index) => (
-                        `${index !== 0 ? "OR" : "WHERE"} checkpointId = ${checkpoint.id}`
-                    ))
-                        }`, function (err, result, fields) {
-                            if (err) throw err;
-                            let sources = result
-                            challenge?.checkpoints.map((checkpoint, index) => {
-                                challenge.checkpoints[index] = sources.filter(a => a.checkpointId === checkpoint.id)
-                            })
-                        });
-                });
-            });
+    new Promise((resolve, reject) => {
+        con.query("SELECT * FROM challenges WHERE id = ?", [req.params['id']], function (err, result, fields) {
+            if (err) reject(err);
+            resolve({ ...result[0] })
         });
-    }).then(() => {
-        res.send(challenge)
+    }).then(response => {
+        Promise.all([
+            new Promise((resolve, reject) => {
+                con.query("SELECT * FROM categories WHERE id = ?", [response.categoryId], function (err, result, fields) {
+                    if (err) reject(err);
+                    resolve({ ...result[0] })
+                });
+            }),
+            new Promise((resolve, reject) => {
+                con.query("SELECT * FROM questions WHERE challengeId = ?", [req.params['id']], function (err, questionResult, fields) {
+                    if (err) reject(err);
+
+                    if (questionResult.length > 0) {
+                        let query = 'SELECT * FROM answers WHERE'
+                        questionResult.map((question, index) => (
+                            query += `${index !== 0 ? ' OR' : ''} questionId=${question.id}`
+                        ))
+
+                        con.query(query, [response.categoryId], function (err, answerResult, fields) {
+                            if (err) reject(err);
+
+                            let questionList = [...questionResult]
+                            questionList.map((question, index) => {
+                                questionList[index].answers = answerResult.filter(a => a.questionId === question.id)
+                                return
+                            })
+
+                            resolve([...questionList])
+
+                        })
+                    } else resolve([...questionResult])
+                });
+            }),
+            new Promise((resolve, reject) => {
+                con.query("SELECT * FROM checkpoints WHERE challengeId = ?", [req.params['id']], function (err, checkpointResult, fields) {
+                    if (err) reject(err);
+
+                    if (checkpointResult.length > 0) {
+                        let query = 'SELECT * FROM sources WHERE'
+                        checkpointResult.map((checkpoint, index) => (
+                            query += `${index !== 0 ? ' OR' : ''} checkpointId=${checkpoint.id}`
+                        ))
+                        con.query(query, [response.categoryId], function (err, sourceResult, fields) {
+                            if (err) reject(err);
+
+                            let checkpointList = [...checkpointResult]
+                            checkpointList.map((checkpoint, index) => {
+                                checkpointList[index].answers = sourceResult.filter(s => s.checkpointId === checkpoint.id)
+                                return
+                            })
+
+                            resolve([...checkpointList])
+
+                        })
+                    } else resolve([...checkpointResult])
+
+                });
+            })
+        ]).then(([
+            category,
+            questions,
+            checkpoints
+        ]) => {
+
+            challenge = {
+                ...challenge,
+                category: category,
+                questions: questions,
+                checkpoints: checkpoints
+            }
+
+            res.send(challenge)
+
+        }).catch(err => {
+
+        })
+    }).catch(err => {
+
     })
 })
 
