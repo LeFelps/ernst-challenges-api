2b635569915a754f4d4ef503a77364ea14334b00
Fellipe Corominas <lefelps@hotmail.com>
2022-08-09 21:27:41 -0300

    User endpoints for userApplications

--- routes/users.js
+++ routes/users.js
@@ -35,9 +35,29 @@ router.put('/', (req, res) => {
     const con = connectDB()
     // TODO 
     // Add all values for users PUT endpoint 
-    con.query(`UPDATE SET users = ? WHERE id = ${user.id}`, [user.languages, req.password], function (err, result, fields) {
+    con.query(`UPDATE users SET fullName, phone, categoryId, level, points, jobPosition, public WHERE id = ${user.id}`, [user.languages, req.password], function (err, result, fields) {
         if (err) throw err;
         res.send(user)
+    });
+})
+
+router.get('/:id', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query(`SELECT * FROM users WHERE id = ${req.prarms['id']}`, function (err, result, fields) {
+        if (err) throw err;
+        res.send(result[0])
+    });
+})
+
+router.get('/', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("SELECT * FROM users", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result)
     });
 })
 
@@ -53,6 +73,101 @@ router.post('/signup', (req, res) => {
             email: reqBody.email,
             lastLogin: Date.now(),
         })
+    });
+})
+
+
+router.post('/apply-job', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("INSERT INTO user_applications (userId, jobId) VALUES (?, ?)", [reqBody.userId, reqBody.jobId], function (err, result, fields) {
+        if (err) throw err;
+        res.send({
+            userId: reqBody.userId,
+            jobId: reqBody.jobId
+        })
+    });
+})
+
+router.get('/apply-job/:id', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("SELECT * from user_applications WHERE userId = ?", [req.params['id']], function (err, result, fields) {
+        if (err) throw err;
+
+        let jobIds = []
+
+        result.map(res => {
+            if (jobIds.indexOf(res.jobId) < 0) {
+                jobIds.push(res.jobId)
+            }
+        })
+
+        if (jobIds.length > 0) {
+            let query = 'SELECT jobs.title, jobs.companyName, jobs.salary, jobs.hideSalary, jobs.level, jobs.compensations, categories.accentColor FROM jobs INNER JOIN categories ON categories.id=jobs.categoryId WHERE'
+
+            jobIds.map((id, index) => {
+                query += `${index !== 0 ? 'OR' : ''} jobs.id = ${id} `
+            })
+
+            con.query(query, [reqBody.userId, reqBody.jobId], function (err, result, fields) {
+                if (err) throw err;
+
+                result.map((job, index) => {
+                    result[index].compensations = job.compensations.split(";")
+                })
+
+                res.send(result)
+            });
+        } else res.send([])
+
+    });
+})
+
+router.delete('/apply-job', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("DELETE * from user_applications WHERE userId = ? AND jobId = ?", [reqBody.userId, reqBody.jobId], function (err, result, fields) {
+        if (err) throw err;
+        res.send(result)
+    });
+})
+
+router.post('/challenge-submit', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("INSERT INTO user_checkpoints (userId, checkpointId, link, completed) VALUES (?, ?)", [reqBody.userId, reqBody.checkpointId, reqBody.link, reqBody.completed], function (err, result, fields) {
+        if (err) throw err;
+        res.send({
+            userId: reqBody.userId,
+            checkpointId: reqBody.checkpointId,
+            link: reqBody.link,
+            completed: reqBody.completed
+        })
+    });
+})
+
+router.get('/challenge-submit', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("SELECT * from user_checkpoints WHERE userId = ?", [req.params['id']], function (err, result, fields) {
+        if (err) throw err;
+        res.send(result)
+    });
+})
+
+router.delete('/challenge-submit', (req, res) => {
+    const con = connectDB()
+
+    const reqBody = req.body
+    con.query("UPDATE user_checkpoints SET link = ?, completed = ? WHERE userId = ? AND checkpointId = ?", [reqBody.link, reqBody.completed, reqBody.userId, reqBody.checkpointId], function (err, result, fields) {
+        if (err) throw err;
+        res.send(result)
     });
 })
 
