8136f5e59c069128f89fe59919076340ece87a71
Fellipe Corominas <lefelps@hotmail.com>
2022-08-10 13:29:00 -0300

    Adjustments to job application

--- routes/jobApplicationRoutes.js
+++ routes/jobApplicationRoutes.js
@@ -18,53 +18,48 @@ router.post('/', (req, res) => {
     });
 })
 
-router.get('/', (req, res) => {
-    const con = connectDB()
-
-    const userId = req.query?.userId
-    const jobId = req.query?.jobId
-
-    const reqBody = req.body
-    con.query("SELECT * FROM user_applications WHERE userId = ? AND jobId", [userId, jobId], function (err, result, fields) {
-        if (err) throw err;
-
-        const response = result.length > 0
-        res.send(response)
-    });
-})
-
 router.get('/:id', (req, res) => {
     const con = connectDB()
 
+    const jobId = req.query?.jobId
+
     const reqBody = req.body
-    con.query("SELECT * from user_applications WHERE userId = ?", [req.params['id']], function (err, result, fields) {
+
+    let query = `SELECT * from user_applications WHERE userId = ${req.params['id']}`
+    if (jobId) query += ` AND jobId = ${jobId}`
+
+    con.query(query, function (err, result, fields) {
         if (err) throw err;
 
-        let jobIds = []
+        if (jobId) res.send(result.length > 0)
+        else {
+            let jobIds = []
 
-        result.map(res => {
-            if (jobIds.indexOf(res.jobId) < 0) {
-                jobIds.push(res.jobId)
-            }
-        })
-
-        if (jobIds.length > 0) {
-            let query = 'SELECT jobs.title, jobs.companyName, jobs.salary, jobs.hideSalary, jobs.level, jobs.compensations, categories.accentColor FROM jobs INNER JOIN categories ON categories.id=jobs.categoryId WHERE'
-
-            jobIds.map((id, index) => {
-                query += `${index !== 0 ? 'OR' : ''} jobs.id = ${id} `
+            result.map(res => {
+                if (jobIds.indexOf(res.jobId) < 0) {
+                    jobIds.push(res.jobId)
+                }
             })
 
-            con.query(query, [reqBody.userId, reqBody.jobId], function (err, result, fields) {
-                if (err) throw err;
+            if (jobIds.length > 0) {
+                let query = 'SELECT jobs.title, jobs.companyName, jobs.salary, jobs.hideSalary, jobs.level, jobs.compensations, categories.accentColor FROM jobs INNER JOIN categories ON categories.id=jobs.categoryId WHERE'
 
-                result.map((job, index) => {
-                    result[index].compensations = job.compensations.split(";")
+                jobIds.map((id, index) => {
+                    query += `${index !== 0 ? 'OR' : ''} jobs.id = ${id} `
                 })
 
-                res.send(result)
-            });
-        } else res.send([])
+                con.query(query, [reqBody.userId, reqBody.jobId], function (err, result, fields) {
+                    if (err) throw err;
+
+                    result.map((job, index) => {
+                        result[index].compensations = job.compensations.split(";")
+                    })
+
+                    res.send(result)
+                });
+            } else res.send([])
+        }
+
 
     });
 })
@@ -73,7 +68,7 @@ router.delete('/', (req, res) => {
     const con = connectDB()
 
     const reqBody = req.body
-    con.query("DELETE * from user_applications WHERE userId = ? AND jobId = ?", [reqBody.userId, reqBody.jobId], function (err, result, fields) {
+    con.query("DELETE FROM   user_applications WHERE userId = ? AND jobId = ?", [reqBody.userId, reqBody.jobId], function (err, result, fields) {
         if (err) throw err;
         res.send(result)
     });
